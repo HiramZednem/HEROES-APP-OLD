d665ed9cae6c70dd369925678bfc5b5223ef0f7a
Hiram <hculebro@neodata.com.mx>
2023-04-14 12:48:04 -0600

    backend added and readme updated"

--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styles: [`
+    .container{
+      margin: 10px;
+    }
+
+    .toolbar {
+      display: flex;
+      justify-content: space-between;
+    }
+  `]
 })
 export class HomeComponent {
 
