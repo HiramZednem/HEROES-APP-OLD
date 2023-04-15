9e374b06daa4212eccc862cd3a0b9444c766a942
Hiram <hculebro@neodata.com.mx>
2023-04-14 19:03:17 -0600

    listado heroes style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 //Luego modulos
 import { AppRoutingModule } from './app-routing.module';
@@ -9,6 +10,7 @@ import { AppRoutingModule } from './app-routing.module';
 //Componentes
 import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+
 
 @NgModule({
   declarations: [
@@ -18,6 +20,7 @@ import { ErrorPageComponent } from './shared/error-page/error-page.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatLegacyListModule } from '@angular/material/legacy-list';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -11,6 +12,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   exports: [
     MatButtonModule,
+    MatCardModule,
     MatIconModule,
     MatLegacyListModule,
     MatSidenavModule,
